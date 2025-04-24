4e4e56b1c1a90c308f0be9b3e08bca5ba7913c93
a00914048 <adam.motaouakkil@huawei.com>
2025-04-24 10:29:42 -0400

    Clean up and add logs for test case name

--- tools/sk_app/ohos/Window_ohos.cpp
+++ tools/sk_app/ohos/Window_ohos.cpp
@@ -67,32 +67,25 @@ void Window_ohos::initDisplay(OHNativeWindow* window) {
     SkASSERT(window);
     // fBackendType = kNativeGL_BackendType;
     switch (fBackendType) {
-#ifdef SK_VULKAN
             case kVulkan_BackendType:
                 LOGI("Window_ohos::initDisplay Initializing Ganesh Vulkan");
                 fWindowContext = skwindow::MakeVulkanForOhos(
                        window, fRequestedDisplayParams->clone());
                 break;
-#if defined(SK_GRAPHITE) && !defined(SK_DAWN)
             case kGraphiteVulkan_BackendType:
                 LOGI("Window_ohos::initDisplay Initializing Graphite Native Vulkan");
                 fWindowContext = skwindow::MakeGraphiteVulkanForOhos(
                        window, fRequestedDisplayParams->clone());
                 break;
-#endif
-
-#if defined(SK_GRAPHITE) && defined(SK_DAWN)
 
             case kGraphiteDawn_BackendType:
                 LOGI("Window_ohos::initDisplay Initializing Graphite Dawn Vulkan");
                 fWindowContext = skwindow::MakeGraphiteDawnVulkanForOhos(
                        window, fRequestedDisplayParams->clone(), fOhosSkiaApp->fXComponent);
                 break;
-#endif
             default:
                 break;
 
-#endif
     }
 
     this->onBackendCreated();
@@ -109,7 +102,6 @@ void Window_ohos::onInval() {
 }
 
 void Window_ohos::paintIfNeeded() {
-    LOGI("Window_ohos::paintIfNeeded");
     if (fWindowContext) { // Check if initDisplay has already been called
         onPaint();
     } else {
--- tools/viewer/Viewer.cpp
+++ tools/viewer/Viewer.cpp
@@ -1512,6 +1512,7 @@ void Viewer::setCurrentSlide(int slide) {
     fSlides[slide]->load(SkIntToScalar(fWindow->width()) / scaleFactor,
                          SkIntToScalar(fWindow->height()) / scaleFactor);
     fCurrentSlide = slide;
+    LOGI("Current Slide: %{public}s", fSlides[slide]->getName().c_str());
     this->setupCurrentSlide();
 }
 
