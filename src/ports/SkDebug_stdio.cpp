63164c67fd80b3b9af3d3463519f843e3b210922
Pi Luo 84359859 <pi.luo@huawei.com>
2025-12-30 11:45:42 -0500

    ohos: enable SkDebugf on ohos

--- src/ports/SkDebug_stdio.cpp
+++ src/ports/SkDebug_stdio.cpp
@@ -17,10 +17,18 @@
 void SkDebugf(const char format[], ...) {
     va_list args;
     va_start(args, format);
+#if defined(SK_BUILD_FOR_OHOS)
+#if BUILDFLAG(ARKWEB_DFX_LOGGING)
+    char str_printed[256];
+    vsprintf(str_printed, format, args);
+    OHOS::NWeb::HiLogAdapter::PrintLog(OHOS::NWeb::LogLevelAdapter::DEBUG, "%{public}s", str_printed);
+#endif
+#else
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wformat-nonliteral"
     vfprintf(stderr, format, args);
 #pragma GCC diagnostic pop
+#endif
     va_end(args);
 }
 #endif  // !defined(SK_BUILD_FOR_WIN) && !defined(SK_BUILD_FOR_ANDROID)
